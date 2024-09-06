f1db8fae56ab2ae568469c0f246d6437669f1806
hadelacruz <humbertoalexanderdelacruz@gmail.com>
2024-09-05 18:17:20 -0600

    Se agregó ambas versiones del ejercicio 02

---
+++ ejercicio2B.cpp
@@ -0,0 +1,49 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <cmath>
+#define NUM_THREADS 10
+
+void *PrintHello(void *paramID){
+    //Convertir de punt. a void --- de puntero a void
+    int *id;                //Guarda un valor de puntero a entero
+    id = (int *)paramID;    //Cast explicita de void* a int*
+    printf("Hello World thread No. %d\n", *id);
+    pthread_exit(NULL);
+    return NULL;
+}
+
+int main(int argc, char const *argv[])
+{
+    //Identificar hilos
+    pthread_t threadID[NUM_THREADS];
+
+    int rc, t, param[NUM_THREADS];
+
+    // Declarar tambien un atributo tipo pthread_attr_t
+    pthread_attr_t attr;
+
+	// Al usar atributos, inicializar el objeto
+    pthread_attr_init(&attr);
+
+    // Modificar el atributo especifico
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+
+    for (t = 0; t < NUM_THREADS; t++){
+        //Asignar valor al parametro que pasara subrutina
+        param[t]=t;
+        //Creación de hilos
+        rc = pthread_create(&threadID[t], &attr, PrintHello, (void*)&param[t]);
+
+        //Verificar
+        if(rc){
+            printf("Error: codigo reotornado desde pthread_create() %d", rc);
+            exit(-1);
+        }
+    
+        //Join
+        pthread_join(threadID[t], NULL);
+    }
+    pthread_exit(NULL);
+}
+
